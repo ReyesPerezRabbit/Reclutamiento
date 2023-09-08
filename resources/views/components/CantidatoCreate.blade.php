1dde81b36e924155375a735edca5586e4e3fd2c4
ErickReyes <203424@uptapachula.edu.mx>
2023-09-08 16:29:04 -0600

    migracion SQL

--- resources/views/components/CantidatoCreate.blade.php
+++ resources/views/components/CantidatoCreate.blade.php
@@ -5,37 +5,40 @@
 @section('content')
 
 
-    <div class="container vh-75">
-        <div class="row justify-content-center align-items-center h-75">
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
             @auth
 
-                <div class="col-lg-10 mt-5">
+                <div class=" mt-5">
                     <div class="shadow p-4 rounded">
 
                         <form action="{{ route('candidato.guardar') }}" method="POST">
                             @csrf
-                            <h1 class="mb-5">Bienvenidos a Higtech</h1>
+                            <h1 class="mb-5">Registro de Candidatos</h1>
 
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="mb-3">
                                         <label for="nombre" class="form-label">Nombre</label>
-                                        <input type="text" class="form-control" id="nombre" name="name"
-                                            placeholder="Nombre Completo" required>
+                                        <input type="text" class="form-control" id="nombre" name="name" required
+                                            oninput="this.value = this.value.toUpperCase()" placeholder="Nombre Completo"
+                                            required>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="mb-3">
                                         <label for="apellidoP" class="form-label">Apellido Paterno</label>
-                                        <input type="text" class="form-control" id="apellidoP" name="apellidoP"
-                                            placeholder="Apellido Paterno" required>
+                                        <input type="text" class="form-control" id="apellidoP" name="apellidoP" required
+                                            oninput="this.value = this.value.toUpperCase()" placeholder="Apellido Paterno"
+                                            required>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="mb-3">
                                         <label for="apellidoM" class="form-label">Apellido Materno</label>
-                                        <input type="text" class="form-control" id="apellidoM" name="apellidoM"
-                                            placeholder="Apellido Materno" required>
+                                        <input type="text" class="form-control" id="apellidoM" name="apellidoM" required
+                                            oninput="this.value = this.value.toUpperCase()" placeholder="Apellido Materno"
+                                            required>
                                     </div>
                                 </div>
                             </div>
@@ -49,6 +52,7 @@
                                             <option value="casado">Casado</option>
                                             <option value="viudo">Viudo</option>
                                             <option value="soltero">Soltero</option>
+                                            <option value="divorciado">divorciado</option>
                                         </select>
                                     </div>
                                 </div>
@@ -68,8 +72,14 @@
                                         <label for="vacante" class="form-label">Vacante</label>
                                         <select class="form-select" id="vacante" name="vacante" required>
                                             <option value="" disabled selected>Seleciona la opcion</option>
+                                            <option value="Gerente">Gerente</option>
                                             <option value="frontend">Frontend</option>
                                             <option value="backend">Backend</option>
+                                            <option value="PMO">PMO</option>
+                                            <option value="Diseñador UI/UX">Diseñador UI/UX</option>
+                                            <option value="Contador">Contador</option>
+                                            <option value="frontend">Frontend</option>
+
                                         </select>
                                     </div>
                                 </div>
@@ -79,7 +89,8 @@
                                 <div class="col-md-4">
                                     <div class="mb-3">
                                         <label for="fechar" class="form-label">Fecha de registro</label>
-                                        <input type="date" class="form-control" id="fechar" name="fecha_registro" required>
+                                        <input type="date" class="form-control" id="fechar" name="fecha_registro"
+                                            required>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -92,8 +103,8 @@
                                 <div class="col-md-4">
                                     <div class="mb-3">
                                         <label for="domicilio" class="form-label">Domicilio</label>
-                                        <input type="text" class="form-control" id="domicilio" name="domicilio"
-                                            placeholder="Domicilio" required>
+                                        <input type="text" class="form-control" id="domicilio" name="domicilio" required
+                                            oninput="this.value = this.value.toUpperCase()" placeholder="Domicilio" required>
                                     </div>
                                 </div>
                             </div>
@@ -102,15 +113,15 @@
                                 <div class="col-md-6">
                                     <div class="mb-3">
                                         <label for="fechaevaluacion" class="form-label">Fecha de envío de evaluación</label>
-                                        <input type="date" class="form-control" id="fechaevaluacion" name="fecha_evaluacion"
-                                            required>
+                                        <input type="date" class="form-control" id="fechaevaluacion"
+                                            name="fecha_evaluacion" required>
                                     </div>
                                 </div>
                                 <div class="col-md-5">
                                     <div class="mb-3">
                                         <label for="nota" class="form-label">Notas</label>
-                                        <input type="text" class="form-control" id="nota" name="nota"
-                                            placeholder="Notas" required>
+                                        <input type="text" class="form-control" id="nota" name="nota" required
+                                            oninput="this.value = this.value.toUpperCase()" placeholder="Notas" required>
                                     </div>
                                 </div>
                             </div>
--- resources/views/components/ListEvaluations.blade.php
+++ resources/views/components/ListEvaluations.blade.php
@@ -5,81 +5,87 @@
 @section('content')
 
 
-<div class="container-fluid vh-75">
-    <div  class="row justify-content-center align-items-center h-75">
-        @auth
-        <div class=" mt-5">
-            <div class="shadow p-4 rounded">
-                @include('layouts.partials.menssage')
+    <div class="container-fluid vh-75">
+        <div class="row justify-content-center align-items-center h-75">
+            @auth
+                <div class=" mt-5">
+                    <div class="shadow p-4 rounded">
+                        @include('layouts.partials.menssage')
 
-                <div style="display: flex; justify-content: space-between; align-items: center;">
-                    <h1>Lista de Evaluación</h1>
-                    <a type="button" class="btn btn-info" href="{{ route('candidato.guardar') }}">Agregar Candidato</a>
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                            <h1>Lista de Evaluación</h1>
+                            <a type="button" class="btn btn-info" href="{{ route('candidato.guardar') }}">Agregar Candidato</a>
+                        </div>
+
+                        <!-- Barra de búsqueda -->
+                        <div class="input-group mt-3 ">
+                            <input type="text" class="form-control" placeholder="Buscar candidato ....">
+                            <button class="btn btn-outline-secondary" type="button">Buscar</button>
+                        </div>
+
+                        <div class="table-scroll">
+                            <table id="miTabla"
+                                class="table table-responsive table-bordered mt-4 table-scroll table-tall table-extended">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th>Nombre</th>
+                                        <th>Apellido Paterno</th>
+                                        <th>Apellido Materno</th>
+                                        <th>Estado Civil</th>
+                                        <th>Estatus</th>
+                                        <th>Vacante</th>
+                                        <th>Fecha de registro</th>
+                                        <th>Edad</th>
+                                        <th>Domicilio</th>
+                                        <th>Fecha de envio de evaluacion</th>
+                                        <th>Notas</th>
+                                        <th>CV</th>
+                                        <th>Expediente</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                        $counter = 1;
+                                    @endphp
+
+                                    @foreach ($candidatocreate as $listas)
+                                        <tr>
+                                            <th scope="row">{{ $counter++ }}</th>
+                                            <td>{{ $listas->name }}</td>
+                                            <td>{{ $listas->apellidoP }}</td>
+                                            <td>{{ $listas->apellidoM }}</td>
+                                            <td>{{ $listas->estado_civil }}</td>
+                                            <td>{{ $listas->estatus }}</td>
+                                            <td>{{ $listas->vacante }}</td>
+                                            <td>{{ $listas->fecha_registro }}</td>
+                                            <td>{{ $listas->year }}</td>
+                                            <td>{{ $listas->domicilio }}</td>
+                                            <td>{{ $listas->fecha_evaluacion }}</td>
+                                            <td>{{ $listas->nota }}</td>
+                                            <td>{{ $listas->cv }}</td>
+                                            <td>
+                                                <a href="#" class="boton-carpeta">
+                                                    <span class="carpeta-icono">&#128193;</span>
+
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
+            @endauth
 
-                <!-- Barra de búsqueda -->
-                <div class="input-group mt-3 ">
-                    <input type="text" class="form-control" placeholder="Buscar candidato ....">
-                    <button class="btn btn-outline-secondary" type="button">Buscar</button>
+
+            {{-- para los que no están autenticados --}}
+            @guest
+                <div class="text-center">
+                    <p class="mt-4">Para poder registrarse, debe iniciar sesión <a href="/login">aquí</a></p>
                 </div>
-
-                <div class="table-scroll">
-                <table id="miTabla" class="table table-responsive table-bordered mt-4 table-scroll table-tall table-extended">
-                    <thead>
-                        <tr>
-                            <th >Nombre</th>
-                            <th >Apellido Paterno</th>
-                            <th >Apellido Materno</th>
-                            <th >Estado Civil</th>
-                            <th >Estatus</th>
-                            <th >Vacante</th>
-                            <th >Fecha de registro</th>
-                            <th >Edad</th>
-                            <th >Domicilio</th>
-                            <th >Fecha de envio de evaluacion</th>
-                            <th >Notas</th>
-                            <th >CV</th>
-                            <th>Expediente</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        @foreach ($candidatocreate as $listas)
-                            <tr>
-                                <td>{{ $listas->name }}</td>
-                                <td>{{ $listas->apellidoP }}</td>
-                                <td>{{ $listas->apellidoM }}</td>
-                                <td>{{ $listas->estado_civil }}</td>
-                                <td>{{ $listas->estatus }}</td>
-                                <td>{{ $listas->vacante }}</td>
-                                <td>{{ $listas->fecha_registro }}</td>
-                                <td>{{ $listas->year }}</td>
-                                <td>{{ $listas->domicilio }}</td>
-                                <td>{{ $listas->fecha_evaluacion }}</td>
-                                <td>{{ $listas->nota }}</td>
-                                <td>{{ $listas->cv }}</td>
-                                <td>
-                                    <a href="#" class="boton-carpeta">
-                                        <span class="carpeta-icono">&#128193;</span>
-
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            </div>
+            @endguest
         </div>
-        @endauth
-
-
-        {{-- para los que no están autenticados --}}
-        @guest
-        <div class="text-center">
-            <p class="mt-4">Para poder registrarse, debe iniciar sesión <a href="/login">aquí</a></p>
-        </div>
-        @endguest
     </div>
-</div>
 @endsection
