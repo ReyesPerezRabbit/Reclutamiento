1577f103a8fb7d58feaf04166e1e2ca53413bdcd
ErickReyes <203424@uptapachula.edu.mx>
2023-09-18 13:59:46 -0600

    bug

--- resources/views/layouts/app_list.blade.php
+++ resources/views/layouts/app_list.blade.php
@@ -44,38 +44,37 @@
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const tabla = document.getElementById("miTabla");
-            const filas = tabla.getElementsByTagName("tr");
-            const inputBusqueda = document.querySelector(".form-control");
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const tabla = document.getElementById("miTabla");
+        const filas = tabla.getElementsByTagName("tr");
+        const inputBusqueda = document.querySelector(".form-control");
 
-            inputBusqueda.addEventListener("keyup", function() {
-                const textoBusqueda = inputBusqueda.value.toLowerCase();
+        inputBusqueda.addEventListener("keyup", function() {
+            const textoBusqueda = inputBusqueda.value.toLowerCase();
 
-                for (let i = 1; i < filas.length; i++) {
-                    const fila = filas[i];
-                    const celdas = fila.getElementsByTagName("td");
-                    let coincide = false;
+            for (let i = 1; i < filas.length; i++) {
+                const fila = filas[i];
+                const celdas = fila.getElementsByTagName("td");
+                let coincide = false;
 
-                    for (let j = 0; j < celdas.length; j++) {
-                        const celda = celdas[j];
-                        if (celda.textContent.toLowerCase().includes(textoBusqueda)) {
-                            coincide = true;
-                            break;
-                        }
-                    }
-
-                    if (coincide) {
-                        fila.style.display = "";
-                    } else {
-                        fila.style.display = "none";
+                for (let j = 0; j < celdas.length; j++) {
+                    const celda = celdas[j];
+                    if (celda.textContent.toLowerCase().includes(textoBusqueda)) {
+                        coincide = true;
+                        break;
                     }
                 }
-            });
-        });
-    </script>
 
+                if (coincide) {
+                    fila.style.display = "";
+                } else {
+                    fila.style.display = "none";
+                }
+            }
+        });
+    });
+</script>
 
 
 </body>
